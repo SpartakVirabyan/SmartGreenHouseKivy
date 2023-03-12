9839d744c9efd7779028b2c9852f6d37576b793b
SPARTA INDUSTRIER <Sparta2023S@gmail.com>
2023-03-13 02:04:43 +0400

    android v.1

--- main.py
+++ main.py
@@ -1,75 +1,59 @@
 from kivy.lang import Builder
-from kivy.properties import ObjectProperty
-
 from kivymd.app import MDApp
-from kivymd.uix.floatlayout import MDFloatLayout
-from kivymd.uix.tab import MDTabsBase
-from kivymd.icon_definitions import md_icons
-
-colors = {
-    "Teal": {
-        "200": "#212121",
-        "500": "#212121",
-        "700": "#212121",
-    },
-    "Red": {
-        "200": "#C25554",
-        "500": "#C25554",
-        "700": "#C25554",
-    },
-    "Light": {
-        "StatusBar": "E0E0E0",
-        "AppBar": "#202020",
-        "Background": "#2E3032",
-        "CardsDialogs": "#FFFFFF",
-        "FlatButtonDown": "#CCCCCC",
-    },
-}
 
 
-KV = '''
-MDBoxLayout:
-    orientation: "vertical"
-
-    MDTopAppBar:
-        title: "Custom theme"
-
-    MDTabs:
-        id: tabs
-
-
-<Tab>
-
-    MDIconButton:
-        id: icon
-        icon: root.icon
-        icon_size: "48sp"
-        theme_icon_color: "Custom"
-        icon_color: "white"
-        pos_hint: {"center_x": .5, "center_y": .5}
-'''
-
-
-class Tab(MDFloatLayout, MDTabsBase):
-    '''Class implementing content for a tab.'''
-
-    icon = ObjectProperty()
-
-
-class Example(MDApp):
-    icons = list(md_icons.keys())[15:30]
+class GreenHouse(MDApp):
 
     def build(self):
-        self.theme_cls.colors = colors
-        self.theme_cls.primary_palette = "Teal"
-        self.theme_cls.accent_palette = "Red"
-        return Builder.load_string(KV)
+        self.theme_cls.material_style = "M3"
+        self.theme_cls.theme_style = "Dark"
+        return Builder.load_string(
+            '''
+MDScreen:
+    
 
-    def on_start(self):
-        for name_tab in self.icons:
-            tab = Tab(title="This is " + name_tab, icon=name_tab)
-            self.root.ids.tabs.add_widget(tab)
+    MDBottomNavigation:
+        #panel_color: "#eeeaea"
+        selected_color_background: "blue"
+        text_color_active: "lightgrey"
+
+        MDBottomNavigationItem:
+            name: 'Home'
+            text: 'Home'
+            icon: 'home'
+            md_bg_color: app.theme_cls.primary_light
+            
+            MDLabel:
+                text: 'Home'
+                halign: 'center'
+
+        MDBottomNavigationItem:
+            name: 'Help'
+            text: 'help'
+            icon: 'help'
+            md_bg_color: app.theme_cls.primary_light
+            
+
+            MDLabel:
+                text: 'Help'
+                halign: 'center'
+                
+
+        MDBottomNavigationItem:
+            name: 'Feedback'
+            text: 'Feedback'
+            icon: 'message-alert'
+            md_bg_color: app.theme_cls.primary_light
+
+            MDLabel:
+                text: 'Feedback'
+                halign: 'center'
+    
+'''
+        )
 
 
-Example().run()
 
+
+
+GreenHouse().run()
