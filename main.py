c249e68643fc86890afa6ed065bfe141bde61f46
SPARTA INDUSTRIER <Sparta2023S@gmail.com>
2023-03-14 22:36:49 +0400

    kivy android21

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivy.properties import StringProperty
 
 
 class GreenHouse(MDApp):
-
+    state = StringProperty("start")
     def build(self):
         self.theme_cls.material_style = "M2"
         self.theme_cls.theme_style = "Dark"
-        return Builder.load_string(
-            '''
+        return Builder.load_string(KV)
+
+
+KV='''
 MDScreen:
     
     MDBottomNavigation:
@@ -24,33 +27,46 @@ MDScreen:
             
             
             MDLabel:
-                text: 'Home'
+                
                 halign: 'center'
 
                 FitImage:
                     source: "image python//greenhouse.png"  
-                    x:350
-                    y:360
-                    halign: 'center'    
+                    pos_hint: {"center_x": .5, "center_y": .5}
+                    halign: 'center' 
+                    x: 350
+                    y: 360   
                     size: "110dp", "110dp"
-                    
-            MDFillRoundFlatIconButton:
+
+            MDRectangleFlatIconButton:
                 icon: "image python//icons8-temperature-inside-64.png"
                 text: "Heater"
                 pos_hint: {"center_x": .5, "center_y": .5}
+                font_size: "20sp"
+                md_bg_color: app.theme_cls.primary_dark
+                text_color: "white"
                 
-            MDFillRoundFlatIconButton:
+            MDRectangleFlatIconButton:
                 icon: "image python//external-Cooler-propeller-others-inmotus-design-15.png"
                 text: "Cooler"
                 pos_hint: {"center_x": .5, "center_y": .4}
-            MDFillRoundFlatIconButton:
+                font_size: "22sp"
+                md_bg_color: app.theme_cls.primary_dark
+                text_color: "white"
+            MDRectangleFlatIconButton:
                 icon: "image python//icons8-thirst-50.png"
                 text: "Water"
                 pos_hint: {"center_x": .5, "center_y": .3}
-            MDFillRoundFlatIconButton:
+                font_size: "24sp"
+                md_bg_color: app.theme_cls.primary_dark
+                text_color: "white"
+            MDRectangleFlatIconButton:
                 icon: "image python//icons8-ozone-structure-64.png"
                 text: "Ozon"
                 pos_hint: {"center_x": .5, "center_y": .2}
+                font_size: "27sp" 
+                md_bg_color: app.theme_cls.primary_dark
+                text_color: "white"
             
         MDBottomNavigationItem:
             name: 'Value'
@@ -86,8 +102,8 @@ MDScreen:
                 
 
     
+
 '''
-        )
 
 
 
