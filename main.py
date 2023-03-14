64d7abcca35b24bc843925c1625131feddd18a43
Spartak <spartak.virabyan.2005@mail.ru>
2023-03-14 14:31:13 +0300

    add icons

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ from kivymd.app import MDApp
 class GreenHouse(MDApp):
 
     def build(self):
-        self.theme_cls.material_style = "M3"
+        self.theme_cls.material_style = "M2"
         self.theme_cls.theme_style = "Dark"
         return Builder.load_string(
             '''
 MDScreen:
     
-
     MDBottomNavigation:
         #panel_color: "#eeeaea"
         selected_color_background: "blue"
@@ -20,29 +19,55 @@ MDScreen:
         MDBottomNavigationItem:
             name: 'Home'
             text: 'Home'
-            icon: 'home'
+            icon: 'image python//icons8-home-64.png'
             md_bg_color: app.theme_cls.primary_light
             
             MDLabel:
                 text: 'Home'
                 halign: 'center'
+            MDFillRoundFlatIconButton:
+                icon: "image python//icons8-temperature-inside-64.png"
+                text: "Heater"
+                pos_hint: {"center_x": .5, "center_y": .5}
+            MDFillRoundFlatIconButton:
+                icon: "image python//external-Cooler-propeller-others-inmotus-design-15.png"
+                text: "Cooler"
+                pos_hint: {"center_x": .5, "center_y": .4}
+            MDFillRoundFlatIconButton:
+                icon: "image python//icons8-thirst-50.png"
+                text: "Water"
+                pos_hint: {"center_x": .5, "center_y": .3}
+            MDFillRoundFlatIconButton:
+                icon: "image python//icons8-ozone-structure-64.png"
+                text: "Ozon"
+                pos_hint: {"center_x": .5, "center_y": .2}
+            
+        MDBottomNavigationItem:
+            name: 'Value'
+            text: 'Value'
+            icon: 'image python//icon_value.png'
+            md_bg_color: app.theme_cls.primary_light
+            
+
+            MDLabel:
+                text: 'Value'
+                halign: 'center'    
 
         MDBottomNavigationItem:
             name: 'Help'
             text: 'help'
-            icon: 'help'
+            icon: 'image python//help-icon-3.png'
             md_bg_color: app.theme_cls.primary_light
             
 
             MDLabel:
                 text: 'Help'
-                halign: 'center'
-                
+                halign: 'center'  
 
         MDBottomNavigationItem:
             name: 'Feedback'
             text: 'Feedback'
-            icon: 'message-alert'
+            icon: 'image python//icons8-comments-64.png'
             md_bg_color: app.theme_cls.primary_light
 
             MDLabel:
