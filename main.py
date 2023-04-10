040d3967d1a25ce773de708dd1c793913906a803
Spartak <spartak.virabyan.2005@mail.ru>
2023-04-10 22:15:53 +0300

    added sensors data

--- main.py
+++ main.py
@@ -8,29 +8,42 @@ from kivymd.uix.menu import MDDropdownMenu
 
 Config.set('graphics', 'width', '400')
 Config.set('graphics', 'height', '650')
-
+cred = credentials.Certificate('smartgreenhouse-a7809-firebase-adminsdk-ltfsz-6e6855dac0.json')
+firebase_admin.initialize_app(cred, {
+            'databaseURL': 'https://smartgreenhouse-a7809-default-rtdb.firebaseio.com/'
+        })
 class MainApp(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.array = ['None']
+        # Set default
+        self.plants = db.reference('/Plants')
+        # Set default
+        self.ref = db.reference('/')
+        for i in self.plants.get():
+            self.array.append(i)
+        self.temperature = db.reference('/Temperature').get()
+        self.humidity = db.reference('/Humidity').get()
+        self.soil = db.reference('/Soil humidity').get()
         self.screen =  Builder.load_file('design.kv')
-        menu_items = [{"text": f"Item {i}"} for i in range(5)]
+        menu_items = [
+            {
+                "text": f"Item {i}",
+                "viewclass": "OneLineListItem",
+                "on_release": lambda x=f"Item {i}": self.menu_callback(x),
+            } for i in range(5)
+        ]
         self.menu = MDDropdownMenu(
             caller=self.screen.ids.button,
             items=menu_items,
-            position="center",
             width_mult=4,
         )
-        self.menu.bind(on_release=self.set_item_menu)
-        self.cred = credentials.Certificate('smartgreenhouse-a7809-firebase-adminsdk-ltfsz-6e6855dac0.json')
-        # Initialize the app with a service account, granting admin privileges
-        firebase_admin.initialize_app(self.cred, {
-            'databaseURL': 'https://smartgreenhouse-a7809-default-rtdb.firebaseio.com/'
-        })
-        # Set default
-        self.ref = db.reference('/')
+        self.screen.ids["temperature"].text = str(self.temperature)
+        self.screen.ids["humidity"].text = str(self.humidity)
+        self.screen.ids["soil"].text = str(self.soil)
 
-    def set_item_menu(self, instance):
-        self.screen.ids.button.set_item(instance.text)
+    def menu_callback(self, text_item):
+        print(text_item)
 
     def build(self):
         self.title = "GreenHouse"
